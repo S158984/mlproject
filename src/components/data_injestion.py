1147cfc65f5db45000d3ad34f292c2025e43b176
S158984 <sudharshan.nalla@gmail.com>
2025-03-14 20:05:55 +0530

    Add files via upload

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -2,6 +2,8 @@ import os
 import sys
 from src.exceptions import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformationClass
+from src.components.data_transformation import DataTransformation
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -9,24 +11,27 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngesionConfig:
-    train_data_path=os.path.join('artifacts','train.csv')
-    test_data_path=os.path.join('artifacts','test.csv')
-    raw_data_path=os.path.join('artifacts','data.csv')
-    print(train_data_path)
+    train_data_path: str=os.path.join('artifacts','train.csv')
+    test_data_path: str=os.path.join('artifacts','test.csv')
+    raw_data_path: str=os.path.join('artifacts','data.csv')
+    print('+++++++++++',train_data_path)
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngesionConfig()
-        print('------>',self.ingestion_config.train_data_path)
+        print('------>')##,self.ingestion_config.train_data_path)
 
     def initiate_data_ingestion(self):
         logging.info('insdie data reading')
+        logging.info('------>>>>>>>  insdie data reading')
         try:
-            df1=pd.read_csv("mlproject\notebook\data\stud.csv")
-            logging.info('data read successfully')  
+            df1=pd.read_csv("src\\notebook\\data\\stud.csv")
+            logging.info('data read successfully') 
+            #print('--->',os.path.dirname()) 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             os.makedirs(os.path.dirname(self.ingestion_config.test_data_path),exist_ok=True)
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            os.makedirs('TestDir',exist_ok=True)
 
             df1.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info('data saved successfully')
@@ -43,9 +48,14 @@ class DataIngestion:
 
             )
         except Exception as e:
-            raise CustomException(e)
+            raise CustomException(e,sys)
 
 
-if __name__=="main":
+if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion
+    train_data,test_data= obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+   
